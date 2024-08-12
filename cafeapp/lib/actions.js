b16dc0dd0587910990a71f9b6d61d901c1b1a8c4
engrabdulmalik <engrabdulmaliq@gmail.com>
2024-08-12 09:53:55 +0500

    new changes

--- cafeapp/lib/actions.js
+++ cafeapp/lib/actions.js
@@ -1,37 +1,9 @@
 "use server";
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
-import * as yup from "yup";
 
 // Define a validation schema
-const mealSchema = yup.object().shape({
-  title: yup
-    .string()
-    .min(3, "Title must be at least 3 characters long")
-    .required("Title is required"),
-  summary: yup
-    .string()
-    .min(4, "Summary must be at least 10 characters long")
-    .required("Summary is required"),
-  instructions: yup
-    .string()
-    .min(4, "Instructions must be at least 10 characters long")
-    .required("Instructions are required"),
-  creator: yup.string().required("Creator is required"),
-  creator_email: yup
-    .string()
-    .email("Invalid email format")
-    .required("Creator email is required"),
-  price: yup
-    .number()
-    .positive("Price must be a positive number")
-    .required("Price is required"),
-  image: yup.string().required("Image is required"),
-});
-
 export async function shareMeal(formData) {
-  console.log("formData", formData);
-  // Create the meal object from FormData
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -43,16 +15,13 @@ export async function shareMeal(formData) {
   };
 
   try {
-    // Validate meal data
-    await mealSchema.validate(meal, { abortEarly: false });
-
     // Save the meal to your database
     await saveMeal(meal);
 
     // Redirect after successful save
     redirect("/meals");
   } catch (err) {
-    // Handle validation errors
+    // Handle errors
     return { errors: err.errors };
   }
 }
--- cafeapp/lib/meals.js
+++ cafeapp/lib/meals.js
@@ -43,29 +43,10 @@ export const getMealBySlug = async (slug) => {
 };
 
 export async function saveMeal(meal) {
-    const slug = slugify(meal.title, { lower: true });
+  const slug = slugify(meal.title, { lower: true });
 
-    // Check if a meal with the same slug already exists
-    // let existingMeal = await db("meals").where({ slug }).first();
-
-    // // If a meal with the same slug exists, generate a unique slug
-    // if (existingMeal) {
-    //   let counter = 1;
-    //   let newSlug = `${slug}-${counter}`;
-    //   while (existingMeal) {
-    //     existingMeal = await db("meals").where({ slug: newSlug }).first();
-    //     if (!existingMeal) {
-    //       slug = newSlug;
-    //       break;
-    //     }
-    //     counter++;
-    //     newSlug = `${slug}-${counter}`;
-    //   }
-    // }
-
-    meal.slug = slug;
-    meal.instructions = xss(meal.instructions);
- 
+  meal.slug = slug;
+  meal.instructions = xss(meal.instructions);
 
   const extension = meal.image.name.split(".").pop();
   const fileName = `${slug}.${extension}`;
